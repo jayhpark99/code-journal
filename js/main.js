c7041390f0cca60073f48a43a3d860e7f4871c57
Jay Park <jayhpark99@gmail.com>
2021-10-27 10:54:51 -0700

    tiny change

--- js/main.js
+++ js/main.js
@@ -70,6 +70,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
   var previousView = localStorage.getItem('data-view');
   if (previousView === 'entries') {
     changeView('entries');
+    data.view = 'entries';
   }
 }
 );

603ee53cb1a23e48f866c6faa308b076a3f1273c
Jay Park <jayhpark99@gmail.com>
2021-10-28 16:01:02 -0700

    changes made v2

--- js/data.js
+++ js/data.js
@@ -4,7 +4,8 @@ var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
-  nextEntryId: 1
+  nextEntryId: 1,
+  color: '#562b81'
 };
 
 window.addEventListener('beforeunload', handleUnload);

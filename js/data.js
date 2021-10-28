5c9cd56ddb54fa433079dd0deabcbe97443a0818
Jay Park <jayhpark99@gmail.com>
2021-10-28 15:53:34 -0700

    changes made

--- js/data.js
+++ js/data.js
@@ -7,13 +7,11 @@ var data = {
   nextEntryId: 1
 };
 
-var $colorPick = document.querySelector('#color-pick');
 window.addEventListener('beforeunload', handleUnload);
 function handleUnload(event) {
   var myStorage = window.localStorage;
   var JSONdata = JSON.stringify(data);
   myStorage.setItem('javascript-local-storage', JSONdata);
-  myStorage.setItem('color', $colorPick.value);
 }
 
 var previousData = localStorage.getItem('javascript-local-storage');
